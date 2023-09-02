109112cfc40de55cce3eab1a0f4e68677b0238f4
farhanali waseer <59307895+farhanwaseer@users.noreply.github.com>
2023-09-02 13:32:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ export default function App() {
   return (
     <div className="App">
       <h1>Farhan Waseer</h1>
-      <h1>Over Menu </h1>   
+      <h1>Over Menu </h1>
+      <h2>Falafa</h2>
+      <p>Chickpea, herbs and spices.</p>
+      {/* <img src="falafel.jpeg" width="240" height="300" alt="A falafel"> */}
+        <h2>Pasta Salad</h2>
+        <p>Lettuce, vegetables and mozzarella.</p>
+        {/* <img src="salad.jpeg" width="240" height="300" alt="A salad">    */}
     </div>
   );
 }
